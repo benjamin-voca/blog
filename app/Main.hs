24c6244b0a98743e824ff47e8d326e10249a38ae
benjamin-voca <benjaminvocaa@gmail.com>
2025-02-01 20:43:53 +0100

    init

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,27 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.Vector (Vector)
+import Data.Vector qualified as V
+import MyLib (someFunc)
+
+data Point = Pass | Portal Int
+
+type Path = Vector Point
+
+step :: Point -> Int
+step p = case p of
+  Pass -> 1
+  (Portal n) -> -n
+
+walk :: Path -> (Point -> Int) -> Int
+walk path f =
+  let len = length path
+  in undefined
+  -- where
+  -- helper :: Path -> (Point -> Int) -> Int -> Int
+  -- helper p f i = 
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  someFunc
